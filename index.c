aec924f615e349ec7e9c8cfe8ef299dd25d4aa62
Ashu Sharma <mrmrashusharma@gmail.com>
2023-04-17 11:42:28 +0530

    Design Changes

--- index.c
+++ index.c
@@ -214,7 +214,7 @@ int main(int argc, char ** argv)
 	GtkApplication *app;
 	int ret;
 	// Giving the application a unique ID to assosiate with
-	app = gtk_application_new("org.ashu", G_APPLICATION_FLAGS_NONE);
+	app = gtk_application_new("org.ashu", G_APPLICATION_DEFAULT_FLAGS);
 	g_signal_connect (app, "activate", G_CALLBACK(activate), NULL);
 	ret = g_application_run(G_APPLICATION(app), argc, argv);
 
