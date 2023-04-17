76effedb448476d60665b97f5c9a049edb9b55eb
Ashu Sharma <mrmrashusharma@gmail.com>
2023-04-17 12:55:09 +0530

    Window-Title-Design-Changes

---
+++ window.c
@@ -0,0 +1,11 @@
+#include <gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+    GtkWidget *window;
+    gtk_init(&argc, &argv);
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "UV with Y variable");
+    gtk_widget_show_all(window);
+    gtk_main();
+    return 0;
+}
